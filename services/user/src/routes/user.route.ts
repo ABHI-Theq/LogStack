d6d0dee21d8bde731ebe6021b1dd1e573a834466
ABHI-Theq <abhi03085e@gmail.com>
2025-08-09 20:23:22 +0530

    author setup and bugs fixes

--- services/user/src/routes/user.route.ts
+++ services/user/src/routes/user.route.ts
@@ -1,6 +1,7 @@
 import {Router} from "express"
-import { getUserById, LoginUser, myProfile, updateUserById } from "../Controllers/user.controller"
+import { getUserById, LoginUser, myProfile, updateProfilePic, updateUserById } from "../Controllers/user.controller"
 import { isAuth } from "../middleware/isAuth"
+import uploadFile from "../middleware/multer"
 
 const router=Router()
 
@@ -8,5 +9,6 @@ router.post("/signin",LoginUser)
 router.get("/me",isAuth,myProfile)
 router.get("/user/:id",isAuth,getUserById)
 router.post("user/update",isAuth,updateUserById)
+router.post("/user/update/pic", isAuth, uploadFile, updateProfilePic);
 
 export default router
